2cf6a0e4bf960ca303c25ba98eed513f408ddb8e
Krish R <krishna.ramanan@gmail.com>
2023-12-30 08:02:54 -0500

    Added some dynamic content to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom";
 
 const App = () => {
-    return <div>Hello React</div>;
+    return (
+      <div>
+        <h1>Hello React</h1>
+        {Math.random() > 0.5 && <h2> Hello JSX </h2>}
+      </div>
+    );
   };
   
   const container = document.getElementById("app");
